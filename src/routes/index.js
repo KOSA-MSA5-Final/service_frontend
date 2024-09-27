1c94ca1eab0112346e46f98f2bbf8016b9cc1ae0
EllieChoi1998 <ellie19981008@gmail.com>
2024-09-27 16:32:28 +0900

    v0

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import BeforeLoginMain from "@/components/BeforeLogin/BeforeLoginMain.vue";
-// import AfterLoginMain from "@/components/AfterLogin/AfterLoginMain.vue";
 import LoginPage from '@/components/BeforeLogin/LoginPage.vue';
 import SignupPage from '@/components/BeforeLogin/SignupPage.vue';
 import AfterLoginMain from '@/components/AfterLogin/Layouts/AfterLoginMain.vue';
@@ -22,15 +21,13 @@ const routes = [
         path: '/main',
         name: 'AfterLoginMain',
         component: AfterLoginMain,
-
         beforeEnter: (to, from, next) => {
             if (isLoggedIn()) {
                 next(); // 로그인된 경우 접근 허용
             } else {
                 next('/login'); // 로그인되지 않은 경우 로그인 페이지로 리다이렉트
             }
-        }
-
+        },
         children: [
             {
                 path: 'profile',
