713759b5c7269e705aa5edc7d74804e6809af85e
SunjuNa <141652391+SunjuNa@users.noreply.github.com>
2024-09-27 19:23:54 +0900

    프로필설정페이지 first page

--- src/components/BeforeLogin/SignupPage.vue
+++ src/components/BeforeLogin/SignupPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="padding:10px;">
     <div class="container-fluid row justify-content-center align-content-center" style="width:auto; margin-bottom:20px;">
-      <div style="display:flex; align-items: start; background-color: white; border-bottom: 1px solid #ebebeb; padding-left:2px;">
+      <div style="display:flex; align-items: start; background-color: white; border-bottom: 1px solid #ebebeb; padding-left:2px;" @click="goToBeforeLogin">
         <div class="back_btn" style="margin-right:10px; align-items: center; display:flex; justify-content: center; justify-items: center; height:40px;">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
@@ -70,6 +70,9 @@ export default {
         .catch(error => {
           console.error("회원가입 실패", error);
         });
+    },
+    goToBeforeLogin(){
+      this.$router.push('/');
     }
   }
 };
