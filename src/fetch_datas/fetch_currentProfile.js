55e5eea44836d1d17f5d7eb6d900b1d0a41465cb
EllieChoi1998 <ellie19981008@gmail.com>
2024-10-03 16:30:12 +0900

    유저 모든 정보 ( Member 및 프로필들) 와 현재 프로필 가져오는 axios 함수 생성 완료

---
+++ src/fetch_datas/fetch_currentProfile.js
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+// 유저의 Member 정보와 모든 프로필들을 가져오는 함수
+
+export const fetch_currentProfile = defineStore('currentProfile', {
+    state: () => ({
+        contents: [], // 여기에 메시지를 받아오기 위해 다른 state를 추가할 수도 있음.
+    }),
+    actions: {
+        async fetchContents() {
+            try {
+                const response = await axios.get('https://localhost:8081/auth/currentProfileInfo', {
+                    headers: {
+                        Authorization: `Bearer ${token}`, // Authorization 헤더에 토큰 추가
+                    },
+                });
+                this.contents = response.data; // 데이터 저장
+                console.log('Fetched content:', this.contents); // 확인용 로그
+            } catch (err) {
+                console.error('Fetch ERROR!', err);
+            }
+        },
+    },
+});
